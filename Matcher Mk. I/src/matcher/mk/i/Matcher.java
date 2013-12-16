873e9090620400bf86467cd993809ef57a144f26
husseinhoussein <houssein.hussein@gmail.com>
2013-12-16 13:07:55 +0100

    Problemen met de opbouw, help

--- Matcher Mk. I/src/matcher/mk/i/Matcher.java
+++ Matcher Mk. I/src/matcher/mk/i/Matcher.java
@@ -4,59 +4,73 @@
  */
 package matcher.mk.i;
 
-import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
-
 /**
  *
  * @author Ikkileo
  */
-public class Matcher extends Object{
+public class Matcher{
 
-String emailRegEx = "(\w+)@(\w+\.)(\w+)(\.\w+)*";
+String emailRegEx = "(\\w+)@(\\w+\\.)(\\w+)(\\.\\w+)*";
 
 Pattern pattern = Pattern.compile(emailRegEx);
-implements MatchResult {
-    Pattern p = Pattern.compile("a*b");
-    java.util.regex.Matcher m = p.matcher("aaaaab");
- boolean b = m.matches();
 
-    @Override
-    public int start() {
+ 
+ 
+ String targetString = "You can email me at g_andy@example.com or andy@example.net to get more info";
+
+private java.util.regex.Matcher matcher; {
+
+ matcher = pattern.matcher(targetString);
+ 
+ while(matcher.find()) {
+	 System.out.println("Found a Match" + Matcher.group());
+	 System.out.println("Start position: " + Matcher.start()); 
+     System.out.println("End position: " + Matcher.end()); 
+ }
+ 
+//implements MatchResult {
+  //  Pattern p = Pattern.compile("a*b");
+ //   java.util.regex.Matcher m = p.matcher("aaaaab");
+// boolean b = m.matches();
+//}
+    
+public static int start() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
+    
     public int start(int group) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
-    public int end() {
+    
+    public static int end() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
+    
     public int end(int group) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
-    public String group() {
+    public static String group() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
-    public String group(int group) {
+    
+    public String group (int group) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
+    
     public int groupCount() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
-}}
+}
+
+}
 // testetsteststetststestests hoi 1337
 // (>O.O)> l33t x infity
 // rainbow bunchie
